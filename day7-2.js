124bcab9d32a32bff6467123dbc34b227a34689e
Miro Metsänheimo <miro@metsanheimo.fi>
2019-12-09 19:57:14 +0200

    finish day 7, properly init babel

--- day7-2.js
+++ day7-2.js
@@ -2,169 +2,157 @@ import data from './day7-data'
 
 class Opcode {
 	constructor(code) {
-		code = '' + code
-		this.code = +code.substring(code.length - 2)
+		code = '' + code;
+		this.code = +code.substring(code.length - 2);
 		this.modes = [...code.substring(0, code.length - 2).padStart(3, '0')]
-				.reverse()
+				.reverse();
 	}
 }
 
 class IntcodeComputer {
 	constructor(data, phase) {
-		this.data = [...data]
-		this.phase = +phase
-		this.output = undefined
-		this.cursor = 0
+		this.data = [...data];
+		this.cursor = 0;
+		this.phase = +phase;
+		this.phaseSettingUsed = false;
 
-		this.run.bind(this)
-		this.handle1.bind(this)
-		this.handle2.bind(this)
-		this.handle3.bind(this)
-		this.handle4.bind(this)
-		this.handle5.bind(this)
-		this.handle6.bind(this)
-		this.handle7.bind(this)
-		this.handle8.bind(this)
-		this.get.bind(this)
-		this.index.bind(this)
+		this.run.bind(this);
 	}
 
 	*run() {
-		this.cursor = 0
-		let phaseUsed = false
 		while (this.cursor < this.data.length && this.data[this.cursor] != 99) {
-			const opcode = new Opcode(this.data[this.cursor])
+			const opcode = new Opcode(this.data[this.cursor]);
 			switch (opcode.code) {
 				case 1:
-					this.handle1(opcode.modes)
-					break
+					this.handle1(opcode.modes);
+					break;
 				case 2:
-					this.handle2(opcode.modes)
-					break
+					this.handle2(opcode.modes);
+					break;
 				case 3:
-					if (phaseUsed) {
-						this.handle3(yield)
+					if (this.phaseSettingUsed) {
+						this.handle3(yield);
 					} else {
-						this.handle3(this.phase)
-						this.phaseUsed = true
+						this.handle3(this.phase);
+						this.phaseSettingUsed = true;
 					}
-					break
+					break;
 				case 4:
-					const output = this.handle4(opcode.modes)
-					this.cursor = 0
-					yield output
+					yield this.handle4(opcode.modes);
+					break;
 				case 5:
-					this.handle5(opcode.modes)
-					break
+					this.handle5(opcode.modes);
+					break;
 				case 6:
-					this.handle6(opcode.modes)
-					break
+					this.handle6(opcode.modes);
+					break;
 				case 7:
-					this.handle7(opcode.modes)
-					break
+					this.handle7(opcode.modes);
+					break;
 				case 8:
-					this.handle8(opcode.modes)
-					break
+					this.handle8(opcode.modes);
+					break;
 				default:
-					throw `Something went wrong, unknown opcode ${opcode.code} at cursor ${this.cursor}`
+					throw `Something went wrong, unknown opcode ${opcode.code} at cursor ${this.cursor}`;
 			}
 		}
 	}
 
-	handle1(modes) {
-		const sum = this.get(modes, 0) + this.get(modes, 1)
-		this.data[this.data[this.cursor + 3]] = sum
-		this.cursor += 4
+	handle1 = (modes) => {
+		const sum = this.get(modes, 0) + this.get(modes, 1);
+		this.data[this.data[this.cursor + 3]] = sum;
+		this.cursor += 4;
 	}
 
-	handle2(modes) {
-		const mult = this.get(modes, 0) * this.get(modes, 1)
-		this.data[this.data[this.cursor + 3]] = mult
-		this.cursor += 4
+	handle2 = (modes) => {
+		const mult = this.get(modes, 0) * this.get(modes, 1);
+		this.data[this.data[this.cursor + 3]] = mult;
+		this.cursor += 4;
 	}
 
-	handle3(input) {
-		this.data[this.data[this.cursor + 1]] = input
-		this.cursor += 2
+	handle3 = (input) => {
+		this.data[this.data[this.cursor + 1]] = input;
+		this.cursor += 2;
 	}
 
-	handle4(modes) {
-		const output = this.get(modes, 0)
-		this.cursor += 2
-		return output
+	handle4 = (modes) => {
+		const output = this.get(modes, 0);
+		this.cursor += 2;
+		return output;
 	}
 
-	handle5(modes) {
+	handle5 = (modes) => {
 		if (this.get(modes, 0) != 0) {
-			this.cursor = this.get(modes, 1)
+			this.cursor = this.get(modes, 1);
 		} else {
-			this.cursor += 3
+			this.cursor += 3;
 		}
 	}
 
-	handle6(modes) {
+	handle6 = (modes) => {
 		if (this.get(modes, 0) == 0) {
-			this.cursor = this.get(modes, 1)
+			this.cursor = this.get(modes, 1);
 		} else {
-			this.cursor += 3
+			this.cursor += 3;
 		}
 	}
 
-	handle7(modes) {
-		const param1 = this.get(modes, 0)
-		const param2 = this.get(modes, 1)
+	handle7 = (modes) => {
+		const param1 = this.get(modes, 0);
+		const param2 = this.get(modes, 1);
 		this.data[this.index(modes[2], this.cursor + 3)] = param1 < param2
-				? 1 : 0
-		this.cursor += 4
+				? 1 : 0;
+		this.cursor += 4;
 	}
 
-	handle8(modes) {
-		const param1 = this.get(modes, 0)
-		const param2 = this.get(modes, 1)
+	handle8 = (modes) => {
+		const param1 = this.get(modes, 0);
+		const param2 = this.get(modes, 1);
 		this.data[this.index(modes[2], this.cursor + 3)] = param1 == param2
-				? 1 : 0
-		this.cursor += 4
+				? 1 : 0;
+		this.cursor += 4;
 	}
 
-	get(modes, modifier) {
-		return this.data[this.index(modes[modifier], this.cursor + modifier + 1)]
+	get = (modes, modifier) => {
+		return this.data[this.index(modes[modifier], this.cursor + modifier + 1)];
 	}
 
-	index(mode, index) {
+	index = (mode, index) => {
 		return mode == 0
 				? this.data[index]
-				: index
+				: index;
 	}
 }
 
-const intcodes = data.split(',').map(i => +i)
+const intcodes = data.split(',').map(i => +i);
 const calculatePermutations = arr => arr.length
 		? arr.reduce((acc, curr, i) => [
 			...acc,
 			...calculatePermutations([ ...arr.slice(0, i), ...arr.slice(i + 1) ])
 				.map(x => [ curr, ...x ])
 			], [])
-		: [[]]
+		: [[]];
 
 const calculateAmplifierValues = (intcodes, phase) => {
 	const amplifiers = [...'ABCDE'].map((_, i) =>
-			new IntcodeComputer(intcodes, phase[i]).run())
-	let result = 0
-	let i = 0
-	let value, done
+			new IntcodeComputer(intcodes, phase[i]).run());
+	let result = 0;
+	let value;
+	let done = false;
 
-	while (!done) {
-		({ value, done } = amplifiers[i % phase.length].next(result))
-		if (value != null) {
-			result = value
+	for (let i = 0; !done; i++) {
+		const next = i % phase.length;
+		({ value, done } = amplifiers[next].next(result));
+		if (value) {
+			result = value;
 		}
-		i++
+		i++;
 	}
 
-	return result
+	return result;
 }
 
 const amplifierValues = calculatePermutations([...'56789'])
-			.map(phase => calculateAmplifierValues(intcodes, phase))
+			.map(phase => calculateAmplifierValues(intcodes, phase));
 
-console.log(`The highest signal that can be sent to the thrusters is ${Math.max(...amplifierValues)}`)
+console.log(`The highest signal that can be sent to the thrusters is ${Math.max(...amplifierValues)}`);
